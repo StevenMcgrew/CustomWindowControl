99f4c8617a741a5ff3745c35991f5470da97d2be
StevenMcgrew <stevenmcgrew8224@msn.com>
2017-04-08 16:39:41 -0700

    Added window transform movement. Organized files and folders.

--- CustomWindowControl/Controls/TemplatedWindowControl.cs
+++ CustomWindowControl/Controls/TemplatedWindowControl.cs
@@ -25,11 +25,13 @@ namespace CustomWindowControl
             this.DefaultStyleKey = typeof(TemplatedWindowControl);
         }
 
+        CompositeTransform transformWindow;
         Button _closeButton;
         Grid _gridRoot;
         ContentPresenter _contentPresenter;
         Rectangle _rectTitleBar;
         Rectangle _rectRight;
+
 
         protected override void OnApplyTemplate()
         {
@@ -39,32 +41,54 @@ namespace CustomWindowControl
             _rectTitleBar = GeneralizedGetTemplateChild<Rectangle>("rectTitleBar");
             _rectRight = GeneralizedGetTemplateChild<Rectangle>("Right");
 
+            this.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
+            _rectTitleBar.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
             _gridRoot.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
             _rectRight.ManipulationMode = ManipulationModes.TranslateX;
 
+            _rectTitleBar.ManipulationDelta += _rectTitleBar_ManipulationDelta;
             _closeButton.Click += _closeButton_Click;
-            _contentPresenter.Loaded += _contentPresenter_Loaded;
             _gridRoot.PointerEntered += _gridRoot_PointerEntered;
             _gridRoot.PointerExited += _gridRoot_PointerExited;
             _rectRight.ManipulationDelta += _rectRight_ManipulationDelta;
             _rectRight.PointerEntered += _rectRight_PointerEntered;
             _rectRight.PointerExited += _rectRight_PointerExited;
 
-            CompositeTransform transformRootGrid = new CompositeTransform();
-            _gridRoot.RenderTransform = transformRootGrid;
-
+            transformWindow = new CompositeTransform();
+            this.RenderTransform = transformWindow;
         }
 
-        private void _contentPresenter_Loaded(object sender, RoutedEventArgs e)
+        private void _rectTitleBar_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
         {
-            //if (_contentPresenter.Content != null)
-            //{
-            //    if (_contentPresenter.Content.GetType().Equals(typeof(Image)))
-            //    {
-            //        _gridRoot.HorizontalAlignment = HorizontalAlignment.Left;
-            //        _gridRoot.VerticalAlignment = VerticalAlignment.Top;
-            //    }
-            //}
+            FrameworkElement prisoner = this;
+            FrameworkElement jail = (Panel)this.Parent;
+
+            // Get the top left point of the prisoner in relationship to the jail
+            GeneralTransform gt = prisoner.TransformToVisual(jail);
+            Point prisonerTopLeftPoint = gt.TransformPoint(new Point(0, 0));
+
+            // Set these variables to represent the edges of the prisoner
+            double left = prisonerTopLeftPoint.X + 9;
+            double top = prisonerTopLeftPoint.Y + 9;
+            double right = left + prisoner.ActualWidth - 18;
+            double bottom = top + prisoner.ActualHeight - 18;
+
+            // Combine those edges with the movement value (When these are used in the next step, it keeps the prisoner from getting stuck at the jail boundary)
+            double leftAdjust = left + e.Delta.Translation.X;
+            double topAdjust = top + e.Delta.Translation.Y;
+            double rightAdjust = right + e.Delta.Translation.X;
+            double bottomAdjust = bottom + e.Delta.Translation.Y;
+
+            // Allow prisoner movement if within jail boundary (Use two separate "if" statements here, so the movement isn't sticky at the boundary)
+            if ((leftAdjust >= 0) && (rightAdjust <= jail.ActualWidth))
+            {
+                transformWindow.TranslateX += e.Delta.Translation.X;
+            }
+
+            if ((topAdjust >= 0) && (bottomAdjust <= jail.ActualHeight))
+            {
+                transformWindow.TranslateY += e.Delta.Translation.Y;
+            }
         }
 
         private void _rectRight_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
@@ -128,7 +152,6 @@ namespace CustomWindowControl
             return child;
         }
 
-
         private void _closeButton_Click(object sender, RoutedEventArgs e)
         {
             ((Panel)this.Parent).Children.Remove(this);
@@ -142,6 +165,5 @@ namespace CustomWindowControl
                                Convert.ToByte(hexColor.Substring(5, 2), 16),
                                Convert.ToByte(hexColor.Substring(7, 2), 16)));
         }
-
     }
 }
